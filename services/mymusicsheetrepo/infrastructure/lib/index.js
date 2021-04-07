31da1c3cba17b35e8cca3a24e332019915ee6941
Konrad Zapotoczny <kzapotoczny06@gmail.com>
2021-04-07 21:01:34 +0200

    Setting up our Serverless infrastructure

---
+++ services/mymusicsheetrepo/infrastructure/lib/index.js
@@ -0,0 +1,12 @@
+import S3Stack from "./S3Stack";
+import DynamoDBStack from "./DynamoDBStack";
+import CognitoStack from "./CognitoStack";
+
+// Add stacks
+export default function main(app) {
+  new DynamoDBStack(app, "dynamodb");
+
+  const s3 = new S3Stack(app, "s3");
+
+  new CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
+}
